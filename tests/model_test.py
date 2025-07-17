8fe40917930bc20fe1263ca8b63b787aa5abb8fc
pedromonnt <pedromonnt@gmail.com>
2025-07-16 23:30:48 -0300

    Refactor model test data preparation and update GitHub workflows for model reporting and API deployment

--- tests/model_test.py
+++ tests/model_test.py
@@ -11,140 +11,131 @@ def prepare_data(data):
 
     data_processed = []
 
-    data_processed.append(int(data["ram_gb"]))
-    data_processed.append(int(data["ssd"]))
-    data_processed.append(int(data["hdd"]))
-    data_processed.append(int(data["graphic_card"]))
-    data_processed.append(int(data["warranty"]))
-
-    data_processed.append(1) if data["brand"] == "asus" else data_processed.append(0)
-    data_processed.append(1) if data["brand"] == "dell" else data_processed.append(0)
-    data_processed.append(1) if data["brand"] == "hp" else data_processed.append(0)
-    data_processed.append(1) if data["brand"] == "lenovo" else data_processed.append(0)
-    data_processed.append(1) if data["brand"] == "other" else data_processed.append(0)
-
-    data_processed.append(1) if data["processor_brand"] == "amd" else data_processed.append(0)
-    data_processed.append(1) if data["processor_brand"] == "intel" else data_processed.append(0)
-    data_processed.append(1) if data["processor_brand"] == "m1" else data_processed.append(0)
-    
-    data_processed.append(1) if data["processor_name"] == "core i3" else data_processed.append(0)
-    data_processed.append(1) if data["processor_name"] == "core i5" else data_processed.append(0)
-    data_processed.append(1) if data["processor_name"] == "core i7" else data_processed.append(0)
-    data_processed.append(1) if data["processor_name"] == "other" else data_processed.append(0)
-    data_processed.append(1) if data["processor_name"] == "ryzen 5" else data_processed.append(0)
-    data_processed.append(1) if data["processor_name"] == "ryzen 7" else data_processed.append(0)
-    
-    data_processed.append(1) if data["os"] == "other" else data_processed.append(0)
-    data_processed.append(1) if data["os"] == "windows" else data_processed.append(0)
-    
-    data_processed.append(1) if data["weight"] == "casual" else data_processed.append(0)
-    data_processed.append(1) if data["weight"] == "gaming" else data_processed.append(0)
-    data_processed.append(1) if data["weight"] == "thinnlight" else data_processed.append(0)
-
-    data_processed.append(1) if data["touchscreen"] == "0" else data_processed.append(0)
-    data_processed.append(1) if data["touchscreen"] == "1" else data_processed.append(0)
-   
-    data_processed.append(1) if data["ram_type"] == "ddr4" else data_processed.append(0)
-    data_processed.append(1) if data["ram_type"] == "other" else data_processed.append(0)
-
-    data_processed.append(1) if data["os_bit"] == "32" else data_processed.append(0)
-    data_processed.append(1) if data["os_bit"] == "64" else data_processed.append(0)
+    data_processed.append(int(data["Month"]))
+    data_processed.append(int(data["Age"]))
+    data_processed.append(int(data["Occupation"]))
+    data_processed.append(float(data["Annual_Income"]))
+    data_processed.append(float(data["Monthly_Inhand_Salary"]))
+    data_processed.append(int(data["Num_Bank_Accounts"]))
+    data_processed.append(int(data["Num_Credit_Card"]))
+    data_processed.append(int(data["Interest_Rate"]))
+    data_processed.append(int(data["Num_of_Loan"]))
+    data_processed.append(int(data["Delay_from_due_date"]))
+    data_processed.append(int(data["Num_of_Delayed_Payment"]))
+    data_processed.append(float(data["Changed_Credit_Limit"]))
+    data_processed.append(int(data["Num_Credit_Inquiries"]))
+    data_processed.append(int(data["Credit_Mix"]))
+    data_processed.append(float(data["Outstanding_Debt"]))
+    data_processed.append(float(data["Credit_Utilization_Ratio"]))
+    data_processed.append(int(data["Credit_History_Age"]))
+    data_processed.append(int(data["Payment_of_Min_Amount"]))
+    data_processed.append(float(data["Total_EMI_per_month"]))
+    data_processed.append(float(data["Amount_invested_monthly"]))
+    data_processed.append(int(data["Payment_Behaviour"]))
+    data_processed.append(float(data["Monthly_Balance"]))
 
     len(data_processed)
 
     return data_processed
 
-def range_golden_data(prediction, expected, tolerance=0.2):
-    lower = expected * (1 - tolerance)
-    upper = expected * (1 + tolerance)
-    return lower < prediction < upper
+def range_golden_data(prediction):
+    lower = 0
+    upper = 2
+    return lower <= prediction <= upper
 
 
 def test_golden_data():
 
     payload = {
-        "brand": "dell",
-        "processor_brand": "intel",
-        "processor_name": "core i5",
-        "os": "windows",
-        "weight": "casual",
-        "warranty": "2",
-        "touchscreen": "0",
-        "ram_gb": "16",
-        "hdd": "0",
-        "ssd": "256",
-        "graphic_card": "8",
-        "ram_type": "ddr4",
-        "os_bit": "64"
+        "Month": "5",
+        "Age": "30",
+        "Occupation": "6",
+        "Annual_Income": "100000",
+        "Monthly_Inhand_Salary": "8000",
+        "Num_Bank_Accounts": "2",
+        "Num_Credit_Card": "4",
+        "Interest_Rate": "10",
+        "Num_of_Loan": "2",
+        "Delay_from_due_date": "4",
+        "Num_of_Delayed_Payment": "11",
+        "Changed_Credit_Limit": "6",
+        "Num_Credit_Inquiries": "3",
+        "Credit_Mix": "2",
+        "Outstanding_Debt": "2000",
+        "Credit_Utilization_Ratio": "30",
+        "Credit_History_Age": "200",
+        "Payment_of_Min_Amount": "2",
+        "Total_EMI_per_month": "50",
+        "Amount_invested_monthly": "250",
+        "Payment_Behaviour": "4",
+        "Monthly_Balance": "400"
     }
-
-    columns = [
-        'ram_gb', 'ssd', 'hdd', 'graphic_card_gb', 'warranty',
-        'brand_asus', 'brand_dell', 'brand_hp', 'brand_lenovo', 'brand_other',
-        'processor_brand_amd', 'processor_brand_intel', 'processor_brand_m1',
-        'processor_name_core i3', 'processor_name_core i5', 'processor_name_core i7',
-        'processor_name_other', 'processor_name_ryzen 5', 'processor_name_ryzen 7',
-        'ram_type_ddr4', 'ram_type_other', 'os_other', 'os_windows',
-        'os_bit_32-bit', 'os_bit_64-bit',
-        'weight_casual', 'weight_gaming', 'weight_thinnlight',
-        'touchscreen_0', 'touchscreen_1'
-    ]
-
-   
+ 
     data_processed = prepare_data(payload)
-    data_processed = np.array([data_processed], dtype=np.int64)
+    data_processed = np.array([data_processed])
 
-
-    df_input = pd.DataFrame(data_processed, columns=columns)
+    df_input = pd.DataFrame(data_processed, columns=payload.keys())
     
+    # Colunas que o modelo espera como inteiros (long)
+    int_columns = ['Occupation', 'Credit_Mix', 'Payment_of_Min_Amount', 'Payment_Behaviour']
+
+    # Converte as colunas para o tipo int64
+    for col in int_columns:
+        df_input[col] = df_input[col].astype(np.int64)
+
     result = model.predict(df_input)
 
     result = int(result[0])
 
     print(f"Prediction: {result}")
 
-    assert range_golden_data(result, 9200), "ensuring golden data range for prediction"
+    assert range_golden_data(result), "ensuring golden data range for prediction"
 
 def test_model_load_call():
 
     payload = {
-        "brand": "dell",
-        "processor_brand": "intel",
-        "processor_name": "core i5",
-        "os": "windows",
-        "weight": "casual",
-        "warranty": "2",
-        "touchscreen": "0",
-        "ram_gb": "16",
-        "hdd": "0",
-        "ssd": "256",
-        "graphic_card": "8",
-        "ram_type": "ddr4",
-        "os_bit": "64"
+        "Month": "5",
+        "Age": "30",
+        "Occupation": "6",
+        "Annual_Income": "100000",
+        "Monthly_Inhand_Salary": "8000",
+        "Num_Bank_Accounts": "2",
+        "Num_Credit_Card": "4",
+        "Interest_Rate": "10",
+        "Num_of_Loan": "2",
+        "Delay_from_due_date": "4",
+        "Num_of_Delayed_Payment": "11",
+        "Changed_Credit_Limit": "6",
+        "Num_Credit_Inquiries": "3",
+        "Credit_Mix": "2",
+        "Outstanding_Debt": "2000",
+        "Credit_Utilization_Ratio": "30",
+        "Credit_History_Age": "200",
+        "Payment_of_Min_Amount": "2",
+        "Total_EMI_per_month": "50",
+        "Amount_invested_monthly": "250",
+        "Payment_Behaviour": "4",
+        "Monthly_Balance": "400"
     }
 
     data_processed = prepare_data(payload)
-    data_processed = np.array([data_processed], dtype=np.int64)
+    data_processed = np.array([data_processed])
 
-    columns = [
-        'ram_gb', 'ssd', 'hdd', 'graphic_card_gb', 'warranty',
-        'brand_asus', 'brand_dell', 'brand_hp', 'brand_lenovo', 'brand_other',
-        'processor_brand_amd', 'processor_brand_intel', 'processor_brand_m1',
-        'processor_name_core i3', 'processor_name_core i5', 'processor_name_core i7',
-        'processor_name_other', 'processor_name_ryzen 5', 'processor_name_ryzen 7',
-        'ram_type_ddr4', 'ram_type_other', 'os_other', 'os_windows',
-        'os_bit_32-bit', 'os_bit_64-bit',
-        'weight_casual', 'weight_gaming', 'weight_thinnlight',
-        'touchscreen_0', 'touchscreen_1'
-    ]
+    df_input = pd.DataFrame(data_processed, columns=payload.keys())
 
-    df_input = pd.DataFrame(data_processed, columns=columns)
+    # Colunas que o modelo espera como inteiros (long)
+    int_columns = ['Occupation', 'Credit_Mix', 'Payment_of_Min_Amount', 'Payment_Behaviour']
+
+    # Converte as colunas para o tipo int64
+    for col in int_columns:
+        df_input[col] = df_input[col].astype(np.int64)
 
     result = model.predict(df_input)
 
     result = int(result[0])
 
     assert isinstance(result, int), "ensuring model prediction returns an integer"
-    assert result > 0, "ensuring model prediction is greater than zero"
+    assert result >= 0, "ensuring model prediction is greater than or equal to zero"
 
 test_golden_data()
+test_model_load_call()
